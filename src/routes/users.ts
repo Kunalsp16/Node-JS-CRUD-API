de3177b8a26eb8d129fc8796e31df8bac1e36efa
Kunalsp16 <kspatil0122@gmail.com>
2023-12-21 13:55:35 +0530

    Update users.ts
    Implemented GET api/users{userId}

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -8,3 +8,20 @@ let users: User[] = [];
 router.get('/',(req: Request, res: Response) => {
     res.status(200).json(users);
 });
+
+// GET api/users/{userId}
+router.get('/:userId', (req: Request, res: Response) => {
+  const userId = req.params.userId;
+
+  if (!userId) {
+    return res.status(400).json({ error: 'Invalid userId' });
+  }
+
+  const user = users.find((u) => u.id === userId);
+
+  if (!user) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+  res.status(200).json(user);
+});
