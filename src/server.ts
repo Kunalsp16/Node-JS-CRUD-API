d53651a673bc09572bd4f1d7ca8020505c7f3221
Kunalsp16 <kspatil0122@gmail.com>
2023-12-23 12:35:40 +0530

    Update server.ts
    exported default app in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
 import userRoutes from './routes/users';
 import { Request, Response, NextFunction } from 'express';
 
-dotenv.config();
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -23,7 +23,14 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
  console.error(err.stack);
  res.status(500).json({ error: 'Internal Server Error' });
   });
-  
+
+
+export default app;
+
 app.listen(port, () => {
- console.log(`Server is running on port ${port}`);
-  });
+    console.log(`Server is running on port ${port}`);
+
+});
+
+
+
